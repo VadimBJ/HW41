744d31834d222244804acf6020c136b9c33fae9a
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-16 17:04:05 +0100

    add parseArguments()

--- src/PizzaRunner.java
+++ src/PizzaRunner.java
@@ -2,13 +2,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
+
 
 public class PizzaRunner {
 
+
+  public static boolean parseArguments(String[] args) {
+    if (args.length != 1) {
+      System.err.println("Некорректное использование!");
+    }
+    try {
+      int maxWeight = Integer.parseInt(args[0]);
+      if (maxWeight < 0) {
+        throw new IncorrectWeightException(maxWeight);
+      }
+      Pizza.setMaxWeight(maxWeight);
+      return true;
+    } catch (NumberFormatException e) {
+      System.err.println("Некорректное использование: " + e.getMessage());
+    }
+    return false;
+  }
+
   public static void main(String[] args) throws IOException {
+    if (parseArguments(args)) {
+      System.out.printf(
+          "\u001B[34mПримечание:\u001B[31m максимально возможный вес %d грамм!\u001B[0m%n",
+          Pizza.getMaxWeight());
+    }
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     try {
       System.out.print("Введите количество пицц: ");
